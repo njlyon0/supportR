401b8518af55698f10fee3ad2e7ede2001737826
njlyon0 <nicholasjlyon@gmail.com>
2022-03-10 14:20:29 -0800

    Added my 'numeric check' function to the package and provided documentation (with examples) for that function

---
+++ R/num_chk.R
@@ -0,0 +1,22 @@
+#' @title Check a Column for Non-Numbers.
+#' @description Any elements in the column that would be changed to NA if `as.numeric()` is used on the column are returned. This is useful for quickly identifying only the "problem" entries of an ostensibly numeric column that is read in as a character.
+#'
+#' @param data A data frame
+#' @param col A column name from the provided data frame as a character vector of one element
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' # Create dataframe with a numeric column where some entries would be coerced into NA
+#' spp <- c('salmon', 'bass', 'halibut', 'eel')
+#' ct <- c(1, '14x', '_23', 12)
+#' fish <- data.frame('species' = spp, 'count' = ct)
+#'
+#' # Use `num_chk()` to return only the entries that would be lost
+#' num_chk(data = fish, col = "count")
+num_chk <- function(data, col) {
+  notNA <- subset(data, !is.na(data[, col]))
+  bad <- subset(notNA, is.na(suppressWarnings(as.numeric(notNA[, col]))))
+  unique(bad[, col])
+}
