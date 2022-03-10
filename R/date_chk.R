d172c12b94882a490df89606039f23980df297d8
njlyon0 <nicholasjlyon@gmail.com>
2022-03-10 14:37:34 -0800

    Added my 'date checker' function to the repository (does the same thing as the numeric checker but for dates)

---
+++ R/date_chk.R
@@ -0,0 +1,22 @@
+#' @title Check a Column for Non-Dates.
+#' @description Any elements in the column that would be changed to NA if `as.Date()` is used on the column are returned. This is useful for quickly identifying only the "problem" entries of an ostensibly date column that is read in as a character.
+#'
+#' @param data A data frame
+#' @param col A column name from the provided data frame as a character vector
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' # Let's create some data that will be useful in demoing this function
+#' loc <- c("LTR", "GIL", "PYN", "RIN")
+#' time <- c('2021-01-01', '2021-01-0w', '1990', '2020-10-xx')
+#' sites <- data.frame('site' = loc, 'visit' = time)
+#'
+#' # Now we can use our function to identify bad dates
+#' date_chk(data = sites, col = 'visit')
+date_chk <- function(data, col) {
+  notNA <- subset(data, !is.na(data[, col]))
+  bad <- subset(notNA, is.na(as.Date(notNA[, col])))
+  unique(bad[, col])
+}
