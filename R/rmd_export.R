1bccca62ed31c98a3f1235e50e2d49098351adff
njlyon0 <nicholasjlyon@gmail.com>
2022-07-13 15:12:53 -0500

    Added informative errors and removed an unneeded argument

--- R/rmd_export.R
+++ R/rmd_export.R
@@ -2,12 +2,11 @@
 #'
 #' @description This function allows you to knit a specified R Markdown file locally and export it to the Google Drive folder for which you provided a link. NOTE that if you have not used `googledrive::drive_auth()` this will prompt you to authorize a Google account in a new browser tab. If you do not check the box in that screen before continuing you will not be able to use this function until you clear your browser cache and re-authenticate. I recommend invoking `drive_auth()` beforehand to reduce the chances of this error
 #'
-#' @param in_path path to the .Rmd file (defaults to `getwd()`)
-#' @param in_name name of .Rmd file (with or without ".Rmd" at end)
-#' @param out_path path to the knit file's destination (defaults to `getwd()`)
-#' @param out_name desired name for knit file (with or without file suffix)
-#' @param out_type either "html" or "pdf" depending on what YAML entry you have in the `output: ` field of your .Rmd
-#' @param drive_link full URL of drive folder to upload the knit document
+#' @param rmd (character) name and path to .Rmd file to knit
+#' @param out_path (character) path to the knit file's destination (defaults to `getwd()`)
+#' @param out_name (character) desired name for knit file (with or without file suffix)
+#' @param out_type (character) either "html" or "pdf" depending on what YAML entry you have in the `output: ` field of your .Rmd
+#' @param drive_link (character) full URL of drive folder to upload the knit document
 #'
 #' @export
 #'
@@ -17,36 +16,51 @@
 #' ## NOTE: See above warning about possible misstep at this stage
 #'
 #' # Use `rmd_export()` to knit and export an .Rmd file
-#' # rmd_export(in_name = "my_markdown.Rmd",
-#' # in_path = file.path("Folder in my WD with the .Rmd named in `in_name`"),
+#' # rmd_export(rmd = "my_markdown.Rmd",
+#' # in_path = file.path("Folder in my WD with the .Rmd named in `rmd`"),
 #' # out_path = file.path("Folder in my WD to save the knit file to"),
 #' # out_name = "desired name for output",
 #' # out_type = "html",
 #' # drive_link = "https://...Google Drive link...")
 #'
-rmd_export <- function(in_path = getwd(), in_name, out_path = getwd(), out_name, out_type = 'html', drive_link) {
+rmd_export <- function(rmd = NULL, out_path = getwd(), out_name = NULL, out_type = 'html', drive_link) {
 
-  # 0 - Reject inappropriate out_type specification
-  if(!out_type %in% c("html", "pdf") | base::length(base::nchar(out_type)) == 0){
-    message("Inappropriate `out_type` specification. Please supply either 'html' or 'pdf'")
-  } else {
+  # Error out for unspecified R markdown file to knit
+  if(base::is.null(rmd))
+    stop("Name of .Rmd file to knit must be provided to `rmd`")
 
-    # 1 - Render Rmarkdown Locally
-    ## If the input name does not include .Rmd, add it
-    if(stringr::str_detect(in_name, pattern = ".Rmd") != "TRUE"){ in_name <- paste0(in_name, ".Rmd") }
+  # Error out for unspecified export name
+  if(base::is.null(out_name))
+    stop("Name to export knit file locally must be provided to `out_name`")
 
-    ## Render provided input
-    rmarkdown::render(input = in_name, output_dir = out_path, output_file = out_name)
-    # 2 - dentify Drive ID from provided link
-    ## Remove sharing part of link if it was provided
+  # Error out if any argument is not a character
+  if(!base::is.character(rmd) |
+     !base::is.character(out_path) | !base::is.character(out_name) |
+     !base::is.character(out_type) | !base::is.character(drive_link))
+    stop("All arguments must be supplied as characters")
+
+  # Error out for invalid export types
+  if(!out_type %in% c("html", "pdf"))
+    stop("Invalid `out_type` specification. Please supply either 'html' or 'pdf'")
+
+    # If the input name does not include .Rmd, add it
+    if(stringr::str_detect(rmd, pattern = ".Rmd") != "TRUE"){ rmd <- base::paste0(rmd, ".Rmd") }
+
+    # Render provided input
+    rmarkdown::render(input = rmd, output_dir = out_path, output_file = out_name)
+
+    # Remove sharing part of link if it was provided
     link_simp <- gsub("\\?usp\\=sharing", "", drive_link)
-    ## Return the link as an ID recognized by `googledrive`
+
+    # Return the link as an ID recognized by `googledrive`
     id <- googledrive::as_id(x = link_simp)
-    # 3 - Check output name for file suffix
+
+    # Check output name for file suffix
     if(stringr::str_detect(out_name, pattern = out_type) != "TRUE"){
-      out_name <- paste(out_name, out_type, sep = ".") }
-    # 4 - Upload knit file to desired location
+      out_name <- base::paste(out_name, out_type, sep = ".") }
+
+    # Upload knit file to desired location
     googledrive::drive_upload(media = file.path(out_path, out_name),
-                              path = id, overwrite = TRUE) }
+                              path = id, overwrite = TRUE)
 }
 
