cd6fd8f5bc0cba9a25167190af8543a618298d31
njlyon0 <nicholasjlyon@gmail.com>
2022-09-07 10:24:07 -0400

    Created a functional version of the function (no pun intended) and added some useful headers to the code

--- R/read_edi.R
+++ R/read_edi.R
@@ -1,3 +1,7 @@
+
+# Housekeeping ----
+
+# Link to my bee data on EDI
 # https://portal.edirepository.org/nis/codeGeneration?packageId=edi.1210.1&statisticalFileType=r
 
 # Load library
@@ -5,6 +9,8 @@ library(tidyverse); library(XML); library(methods)
 
 # Clear environment
 rm(list = ls())
+
+# Non-function workflow ----
 
 # Identify URL
 pasta_link  <- "https://pasta.lternet.edu/package/eml/edi/1210/1"
@@ -67,7 +73,7 @@ object_ids
 edi_list <- list()
 
 # And assign each file in the fixed PASTA identifier to that list
-for(id in object_ids$identifier){
+for(id in object_ids$identifier[1:3]){
 
   # Make a tempfile
   new_temp <- tempfile()
@@ -98,36 +104,114 @@ for(id in object_ids$identifier){
 str(edi_list)
 
 
+# Function v2 ----
 
-i <- 5
+# Clear environment
+rm(list = ls())
 
-# Make a tempfile
-new_temp <- tempfile()
+# Define function
+read_edi <- function(pasta_id = NULL, data_type = "csv"){
+
+  # Error out if URL isn't provided
+  if(is.null(pasta_id)) stop("EDI URL must be provided")
+
+  # Error out if data isn't a CSV
+  if(data_type != "csv") stop("Data must be CSV format")
+
+  # Warning/errors for malformatted pasta_ids?
+
+# Make tempfile to read in
+temp_dest <- tempfile()
 
 # Attempt the download
-try(download.file(url = object_ids$identifier[i],
-                  destfile = new_temp,
-                  method = "curl", quiet = TRUE))
-
-# If that doesn't work, try reading in a different way
-if (is.na(file.size(new_temp)))
-  try(download.file(url = object_ids$identifier[i],
-                    destfile = new_temp,
-                    method = "auto", quiet = TRUE))
+try(download.file(url = pasta_id, destfile = temp_dest,
+                  method = "auto", quiet = TRUE))
 
 # Read that in
-data <- read.csv(file = new_temp)
-glimpse(data)
+pasta_sub_ids <- read.csv(file = temp_dest)
 
+# Rip needed parts
+first_obj <- data.frame("data_name" = names(pasta_sub_ids))
+other_obj <- pasta_sub_ids[1]
+names(other_obj) <- "data_name"
 
+# Bind 'em together
+data_objects <- dplyr::bind_rows(first_obj, other_obj)
 
-#
-#
-#
-#
-#
-#
-library(tidyverse)
+# Prep a template value to fix the first object
+url_prefix <- paste0(gsub(pattern = "package/eml/edi",
+                          replacement = "package/data/eml/edi",
+                          x = data_objects[nrow(data_objects),]), "/")
+
+# Make a dumber version of the prefix
+prefix_simp <- gsub(pattern = "/|:", replacement = ".", x = url_prefix)
+
+# Tweak the object IDs
+object_ids <- data_objects %>%
+  # Strip out good and bad data prefixes
+  dplyr::mutate(data_name_mod = ifelse(
+    test = stringr::str_detect(string = data_name, pattern = prefix_simp),
+    yes = gsub(pattern = prefix_simp, replacement = "", x = data_name),
+    no = data_name)) %>%
+  # Replace with the correct template
+  dplyr::mutate(identifier = ifelse(
+    test = stringr::str_detect(string = data_name_mod, pattern = "https://"),
+    yes = data_name_mod,
+    no = paste0(url_prefix, data_name_mod))) %>%
+  # Retain only fixed identifiers
+  dplyr::select(identifier) %>%
+  # Identify the XML files included with the data
+  dplyr::mutate(data_type = dplyr::case_when(
+    stringr::str_detect(string = identifier, pattern = url_prefix) ~
+      "data",
+    stringr::str_detect(string = identifier, pattern = "metadata") ~ "xml",
+    TRUE ~ 'xxx')) %>%
+  # Drop unidentified file types
+  dplyr::filter(data_type != "xxx")
+
+# Now make an empty list
+edi_list <- list()
+
+# And assign each file in the fixed PASTA identifier to that list
+for(id in object_ids$identifier){
+
+  # Make a tempfile
+  new_temp <- tempfile()
+
+  # Attempt the download
+  try(download.file(url = id, destfile = new_temp,
+                    method = "curl", quiet = TRUE))
+
+  # If that doesn't work, try reading in a different way
+  if (is.na(file.size(new_temp)))
+    try(download.file(url = id, destfile = new_temp,
+                      method = "auto", quiet = TRUE))
+
+  # Read in data objects
+  if(object_ids$data_type[object_ids$identifier == id] == "data"){
+    data <- read.csv(file = new_temp) }
+
+  # If XML, parse the XML instead
+  if(object_ids$data_type[object_ids$identifier == id] == "xml"){
+    data <- XML::xmlParse(file = new_temp) }
+
+  # Whatever they are, add 'em to the list
+  edi_list[[id]] <- data
+
+}
+
+# Return the now filled list
+return(edi_list) }
+
+# Invoke function
+test <- read_edi(pasta_id = "https://pasta.lternet.edu/package/eml/edi/1210/1")
+
+names(test)
+str(test)
+
+# Function v1 ----
+
+rm(list = ls())
 
 # Function equivalent
 read_edi <- function(pasta_id = NULL, data_type = "csv"){
