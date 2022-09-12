d9fd7df4f41417cc152410ac0529a4210afe7bb8
njlyon0 <nicholasjlyon@gmail.com>
2022-09-12 16:20:36 -0400

    First commit of new `summary_table` function

---
+++ R/summary_table.R
@@ -0,0 +1,67 @@
+#' @title Generate Summary Table for Supplied Response and Grouping Variables
+#'
+#' @description Calculates mean, standard deviation, sample size, and standard error of a given response variable within user-defined grouping variables. This is meant as a convenience instead of doing `dplyr::group_by` followed by `dplyr::summarize` iteratively themselves.
+#'
+#' @param data (dataframe or dataframe-like) object with column names that match the values passed to the `groups` and `response` arguments
+#' @param groups (character) vector of column names to group by
+#' @param response (character) name of the column name to calculate summary statistics for (the column must be numeric)
+#' @param round_digits (numeric) number of digits to which mean, standard deviation, and standard error should be rounded
+#'
+#' @return (dataframe) summary table containing the mean, standard deviation, sample size, and standard error of the supplied response variable)
+#'
+#' @importFrom magrittr %>%
+#' @export
+#'
+summary_table <- function(data = NULL, groups = NULL, response = NULL,
+                          round_digits = 2){
+
+  # Error out if anything isn't provided
+  if(is.null(data) | is.null(groups) | is.null(response))
+    stop("All arguments must be specified")
+
+  # Error out if groups or response are not characters
+  if(!is.character(groups) | !is.character(response))
+    stop("Grouping and response variables must be supplied as characters")
+
+  # Error out if groups or response are not column names in the data
+  if(base::length(base::setdiff(x = groups, y = names(data))) != 0 |
+     !response %in% names(data))
+    stop("Grouping and response variables must be identical to column names in data object")
+
+  # Error out if response is non-numeric
+  if(!is.numeric(data[[response]]))
+    stop("Response variable must be numeric")
+
+  # Warn if rounding digits is not a number
+  if(!is.numeric(round_digits)){
+    message("Rounding digits must be a number. Defaulting to 2")
+    round_digits <- 2
+  }
+
+  # Silence `dplr::summarize`
+  options(dplyr.summarise.inform = FALSE)
+
+  # Create a summary table
+  tidy <- data %>%
+    # Group by provided grouping variables
+    dplyr::group_by(dplyr::across(dplyr::all_of(groups))) %>%
+    # Perform summarization
+    dplyr::summarize(
+      mean = base::round(base::mean(!!ensym(response), na.rm = TRUE),
+                         digits = round_digits),
+      std_dev = base::round(stats::sd(!!ensym(response), na.rm = TRUE),
+                            digits = round_digits),
+      sample_size = dplyr::n(),
+      std_error = base::round((std_dev / base::sqrt(sample_size)),
+                              digits = round_digits),
+    ) %>%
+    # Make it a dataframe
+    as.data.frame()
+
+  # Return the tidy data
+  return(tidy) }
+
+# Test it
+summary_table(data = penguins, groups = c("sex", "island"), response = "body_mass_g")
+
+
