55b842dbc0376ffad1c03572c17008aa4af4635c
njlyon0 <njlyon@alumni.iastate.edu>
2023-07-06 16:54:20 -0400

    First working version of function that lists the contents of one folder of a GitHub repo (or the top-level contents)

--- dev/github_ls.R
+++ dev/github_ls.R
@@ -2,42 +2,57 @@
 # Load needed libraries
 library(gh); library(stringr)
 
+# Clear environment
+rm(list = ls())
 
-# Identify a repo URL
-repo_url <- "https://github.com/Traneptora/grimoire"
-folder <- "_includes"
-
-# Strip out owner and repo name
-repo_bits <- stringr::str_split_1(string = gsub(x = repo_url,
-                                                pattern = "https\\:\\/\\/github.com\\/",
-                                                replacement = ""),
-                                  pattern = "/")
-
-# Build first query
-repo_query_raw <- paste("/repos", repo_bits[1], repo_bits[2], "contents", sep = "/")
-
-# Assemble API query
-repo_query <- ifelse(nchar(folder) > 0,
-                     yes = paste0(repo_query_raw, "/", folder),
-                     no = repo_query_raw)
-
-# Process GitHub URL into GitHub API query format
-repo_conts <- gh::gh(endpoint = repo_query)
-
-# Make empty vectors for later use
-repo_values <- NULL
-repo_types <- NULL
-
-# Identify files in that folder
-for(k in 1:length(repo_conts)){
+# Identify all files in a single folder
+github_ls_single <- function(repo, folder = NULL){
   
-  # Strip out vector of file/directory names and types
-  repo_values <- c(repo_values, repo_conts[[k]]$name)
-  repo_types <- c(repo_types, repo_conts[[k]]$type)
+  # Break URL into its component parts
+  url_bits <- stringr::str_split_1(string = repo, pattern = "/")
   
-}
+  # Error out if "github.com" isn't in the URL
+  if(!"github.com" %in% url_bits)
+    stop("`repo` must be the URL for a GitHub repository (including 'github.com')")
+  
+  # Drop unwanted parts of that
+  repo_id <- setdiff(x = url_bits, y = c("https:", "", "github.com"))
+  
+  # Assemble an API query with this information
+  query_raw <- paste("/repos", repo_id[1], repo_id[2], "contents", sep = "/")
+  
+  # Add on the folder if one was provided
+  query_actual <- ifelse(test = is.null(folder) == TRUE,
+                         yes = query_raw,
+                         no = paste0(query_raw, "/", folder))
+  
+  # Make the query to the GitHub API
+  repo_contents <- gh::gh(endpoint = query_actual)
+  
+  # Make empty vectors for later use
+  repo_names <- NULL
+  repo_types <- NULL
+  
+  # Identify files in that folder
+  for(k in 1:length(repo_contents)){
+    
+    # Strip out vector of file/directory names and types
+    repo_names <- c(repo_names, repo_contents[[k]]$name)
+    repo_types <- c(repo_types, repo_contents[[k]]$type)
+    
+  }
+  
+  # Create a dataframe from this
+  repo_df <- data.frame("name" = repo_names,
+                        "type" = repo_types)
+  
+  # Return that dataframe
+  return(repo_df) }
 
-# Create a dataframe from this
-repo_df <- data.frame("name" = repo_values,
-                      "type" = repo_types)
+# Invoke the function
+github_ls_single(repo = "https://github.com/Traneptora/grimoire")
+github_ls_single(repo = "https://github.com/Traneptora/grimoire", folder = "_posts")
+
+
+
 
