d9b5613e29cc416c47e971860468ccce38d5045b
njlyon0 <njlyon@alumni.iastate.edu>
2023-07-10 17:37:07 -0400

    First working version of (admittedly simple) function

--- dev/name_vec.R
+++ dev/name_vec.R
@@ -1,3 +1,23 @@
 
 
-# Make a function that creates a named vector from two vectors instead of first defining an unnamed vector and then coercing its names via `names(vec) <- c(...manual names...)`
+# Make a function that creates a named vector from two vectors instead of first defining an unnamed vector and then coercing its names via `names(vec) <- c(...manual names...)
+name_vec <- function(content, name){
+  
+  # Error out if content & name are not the same length
+  if(length(x = content) != length(x = name))
+    stop("Vector content and names must be the same length")
+  
+  # Construct the named vector
+  names(content) <- name
+  
+  # Return that
+  return(content) }
+
+# Test function
+name_vec(content = 1:10, name = paste0("text_", 1:10))
+
+
+
+
+
+
