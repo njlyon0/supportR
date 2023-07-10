5687b73ed513535e9d7fd1f3ea8612fff6d925f6
njlyon0 <njlyon@alumni.iastate.edu>
2023-07-10 17:59:29 -0400

    First pass at roxygen formatting

--- dev/name_vec.R
+++ dev/name_vec.R
@@ -1,6 +1,18 @@
-
-
-# Make a function that creates a named vector from two vectors instead of first defining an unnamed vector and then coercing its names via `names(vec) <- c(...manual names...)
+#' @title Create Named Vector
+#'
+#' @description Create a named vector in a single line without either manually defining names at the outset (e.g., `c("name_1" = 1, "name_2" = 2, ...`) or spending a second line to assign names to an existing vector (e.g., `names(vec) <- c("name_1", "name_2", ...)`). Useful in cases where you need a named vector within a pipe and don't want to break into two pipes just to define a named vector (see `tidyr::separate_wider_position`)
+#' 
+#' @param content (vector) content of vector
+#' @param name (vector) names to assign to vector (must be in same order )
+#' 
+#' @return (named vector) vector with contents from the `content` argument and names from the `name` argument
+#' 
+#' @export
+#' 
+#' @examples
+#' # Create a named vector
+#' name_vec(content = 1:10, name = paste0("text_", 1:10))
+#' 
 name_vec <- function(content, name){
   
   # Error out if content & name are not the same length
@@ -12,12 +24,3 @@ name_vec <- function(content, name){
   
   # Return that
   return(content) }
-
-# Test function
-name_vec(content = 1:10, name = paste0("text_", 1:10))
-
-
-
-
-
-
