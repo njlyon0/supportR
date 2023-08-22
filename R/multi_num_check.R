f2a21e5b982e18ceb5219bade17cb1c7d8532a7a
njlyon0 <njlyon@alumni.iastate.edu>
2023-08-22 10:03:26 -0400

    Flagging `multi_num_check` as deprecated and expanding `num_check` to fill its role

--- R/multi_num_check.R
+++ R/multi_num_check.R
@@ -1,6 +1,12 @@
 #' @title Check Multiple Columns for Non-Numbers
-#' @description Any elements in the columns that would be changed to NA if `as.numeric` is used are returned. This is useful for quickly identifying only the "problem" entries of ostensibly numeric columns that are read in as characters. This function is an extension of the `num_check` function in this package that deals with only a single column.
-#'
+#' 
+#' @keywords internal
+#' 
+#' @description 
+#' `r lifecycle::badge("deprecated")`
+#' 
+#' This function was deprecated because I realized that it is just a special case of the [num_check()] function.
+#' 
 #' @param data (dataframe) object containing at least one column of supposed numbers
 #' @param col_vec (character or numeric) vector of names or column numbers of the columns containing putative numbers in the data object
 #'
@@ -18,44 +24,12 @@
 #' # Use `multi_num_check()` to return only the entries that would be lost
 #' multi_num_check(data = fish, col_vec = c("count", "num_col2", "third_count"))
 multi_num_check <- function(data = NULL, col_vec = NULL){
-  # Error out if anything is missing
-  if(base::is.null(data) | base::is.null(col))
-    stop("Data object name and column name must be provided")
-
-  # Make an empty list to store the malformed dates in
-  bad_list <- base::list()
-
-  # Make sure the data object is a dataframe
-  df <- base::as.data.frame(data)
-
-  # For each column the column vector...
-  for(col_opt in col_vec) {
-
-    # Remove NA entries
-    notNA <- base::subset(df, !base::is.na(df[, col_opt]))
-
-    # Identify rows that would be lost if `as.numeric()` is used
-    bad_df <- base::subset(notNA,
-                           base::is.na(
-                             base::suppressWarnings(
-                               base::as.numeric(notNA[, col_opt]))))
-
-    # Get a vector of just the unique 'bad' entries
-    bad_vec <- base::unique(bad_df[, col_opt])
-
-    # If that vector is length 0 (i.e., no bad entries)...
-    if(base::length(bad_vec) == 0){
-      # ...print a message saying so
-      message("For '", col_opt, "', no non-numeric values identified.")
-
-      # If there are any bad entries...
-    } else {
-      # ... print the name of the column and all of the bad entries in it
-      message("For '", col_opt, "', ", length(bad_vec),
-              " non-numbers identified: '",
-              paste0(bad_vec, collapse = "' | '"), "'")# And add the values to a list
-      bad_list[[col_opt]] <- bad_vec } }
-
-  # Return the list
-  return(bad_list)
+  
+  # Add deprecation message
+  lifecycle::deprecate_warn(when = "1.1.1", what = "multi_num_check()",
+                            with = "num_check()")
+  
+  # Invoke `num_check` instead
+  num_check(data = data, col = col_vec)
+  
 }
--- R/num_check.R
+++ R/num_check.R
@@ -1,40 +1,65 @@
 #' @title Check a Column for Non-Numbers
-#' @description Any elements in the column that would be changed to NA if `as.numeric` is used on the column are returned. This is useful for quickly identifying only the "problem" entries of an ostensibly numeric column that is read in as a character.
+#' 
+#' @description Any elements in the column(s) that would be changed to NA if `as.numeric` is used on the column(s) are returned. This is useful for quickly identifying only the "problem" entries of an ostensibly numeric column that is read in as a character.
 #'
 #' @param data (dataframe) object containing at least one column of supposed dates
-#' @param col (character or numeric) name or column number of the column containing putative dates in the data object
+#' @param col (character or numeric) name(s) or column number(s) of the column(s) containing putative dates in the data object
 #'
-#' @return (character) vector of malformed dates
+#' @return (list) malformed numbers from each supplied column in separate list elements
 #' @export
 #'
 #' @examples
 #' # Create dataframe with a numeric column where some entries would be coerced into NA
 #' spp <- c('salmon', 'bass', 'halibut', 'eel')
 #' ct <- c(1, '14x', '_23', 12)
-#' fish <- data.frame('species' = spp, 'count' = ct)
+#' ct2 <- c('a', '2', '4', '0')
+#' ct3 <- c(NA, 'Y', 'typo', '2')
+#' fish <- data.frame('species' = spp, 'count' = ct, 'num_col2' = ct2, 'third_count' = ct3)
 #'
 #' # Use `num_check()` to return only the entries that would be lost
-#' num_check(data = fish, col = "count")
+#' num_check(data = fish, col = c("count", "num_col2", "third_count"))
 num_check <- function(data = NULL, col = NULL) {
 
   # Error out if anything is missing
   if(base::is.null(data) | base::is.null(col))
     stop("Data object name and column name must be provided")
 
+  # Make an empty list to store the malformed dates in
+  bad_list <- base::list()
+  
   # Make data a dataframe
   df <- base::as.data.frame(data)
 
-  # Remove NAs
-  notNA <- base::subset(df, !base::is.na(df[, col]))
-
-  # Identify "numbers" that are malformed
-  bad <- base::subset(notNA,
-                      base::is.na(
-                        base::suppressWarnings(
-                          base::as.numeric(notNA[, col]))))
-  # Return a vector of bad numbers
-  if(nrow(bad) != 0){
-    base::unique(bad[, col])
-    # Or a message saying all is well
-  } else { message('No bad numbers.') }
-}
+  # For each supplied column...
+  for(k in 1:length(col)) {
+    
+    # Identify the specific column option
+    col_opt <- col[k]
+    
+    # Remove NA entries
+    notNA <- base::subset(df, !base::is.na(df[, col_opt]))
+    
+    # Identify rows that would be lost if `as.numeric()` is used
+    bad_df <- base::subset(notNA,
+                           base::is.na(
+                             base::suppressWarnings(
+                               base::as.numeric(notNA[, col_opt]))))
+    
+    # Get a vector of just the unique 'bad' entries
+    bad_vec <- base::unique(bad_df[, col_opt])
+    
+    # If that vector is length 0 (i.e., no bad entries)...
+    if(base::length(bad_vec) == 0){
+      # ...print a message saying so
+      message("For '", col_opt, "', no non-numeric values identified.")
+      
+      # If there are any bad entries...
+    } else {
+      # ... print the name of the column and all of the bad entries in it
+      message("For '", col_opt, "', ", length(bad_vec),
+              " non-numbers identified: '",
+              paste0(bad_vec, collapse = "' | '"), "'")# And add the values to a list
+      bad_list[[col_opt]] <- bad_vec } }
+  
+  # Return the list
+  return(bad_list) }
