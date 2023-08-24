aaaeb65b25aad48e32075612ba81aa7e57f10461
njlyon0 <njlyon@alumni.iastate.edu>
2023-08-24 13:05:55 -0400

    Empty script to work on something that makes a neat little plot checking for inter-variable colinearity / correlation

---
+++ dev/colinear_plot.R
@@ -0,0 +1,18 @@
+## ------------------------------------------- ##
+# Colinearity Checker Plot
+## ------------------------------------------- ##
+
+# Load needed packages
+libarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+
+
+
+
+
+
+
+# End ----
