9111599b05f50fe3c0c3aa465af356d288641b7c
njlyon0 <njlyon@alumni.iastate.edu>
2023-08-24 13:57:52 -0400

    Adding more exploratory / charcoal sketch code. This function is still _very_ early in the dev phase

--- dev/colinear_plot.R
+++ dev/colinear_plot.R
@@ -1,17 +1,141 @@
 ## ------------------------------------------- ##
-# Colinearity Checker Plot
+          # Colinearity Checker / Plot
 ## ------------------------------------------- ##
 
 # Load needed packages
-libarian::shelf(tidyverse)
+librarian::shelf(tidyverse, caTools, car, quantmod, MASS, corrplot, supportR, geomtextpath)
 
 # Clear environment
 rm(list = ls())
 
+# Make faux dataframe for testing purposes
+df <- data.frame(score = c(35, 42, 24, 27, 37),
+                 study_hrs = c(5, 4, 2, 3, 4),
+                 play_hrs = c(4, 3, 4, 2, 2),
+                 attendance = c(8, 8, 4, 6, 9) )
+
+# Make a version without the 'response' variable
+(exp_df <- df[2:4])
+
+# Check correlation
+(cor_mat <- stats::cor(x = exp_df, method = "pearson"))
+
+# Wrangle that matrix
+cor_df <- supportR::crop_tri(data = cor_mat, drop_tri = "upper", drop_diag = T) %>%
+  # Make into a dataframe
+  as.data.frame(.) %>%
+  # Get row names into a column
+  dplyr::mutate(var_a = rownames(.)) %>%
+  # Pivot to long format
+  tidyr::pivot_longer(cols = -var_a, names_to = "var_b", values_to = "cor") %>%
+  # Drop unwanted comparisons
+  dplyr::filter(var_a != var_b & !is.na(cor))
+
+# Check that out
+cor_df
+
+# Compare with initial correlation matrix
+cor_mat
+
+
+
+# More exploratory dataframes
+df <- data.frame(x1 = c(seq(0, 10/6 * pi, pi/3),
+                        seq(0, 10/6 * pi, 2*pi/3)),
+                 y1 = c(rep(2, 6), rep(-1, 3)),
+                 x2 = c(seq(0, 10/6 * pi, pi/3)  + pi/3,
+                        seq(0, 10/6 * pi, 2*pi/3) + 2*pi/3),
+                 y2 = c(rep(4, 6), rep(2, 3)),
+                 group = letters[c(1:6, (1:3) * 2)],
+                 alpha = c(rep(1, 6), rep(0.4, 3)))
+
+# Cute squares / rectangles plot
+p <- ggplot(df, aes(x1, y1)) +
+  geom_rect(aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2, fill = group,
+                alpha = alpha),
+            color = "white", size = 2) +
+  geom_textpath(data = data.frame(x1 = seq(0, 2 * pi, length = 300),
+                                  y1 = rep(0.5, 300),
+                                  label = rep(c("stats", "effects", "polar"), each = 100)),
+                aes(label = label), linetype = 0, linewidth = 8,
+                upright = TRUE) +
+  geom_textpath(data = data.frame(x1 = seq(0, 2 * pi, length = 300),
+                                  y1 = rep(3, 300),
+                                  label = rep(c("density", "smooth", "unique", "organic",
+                                                "easy to use", "automatic"), 
+                                              each = 50)),
+                aes(label = label), linetype = 0, linewidth = 4.6, color = "white",
+                upright = TRUE) +
+  scale_y_continuous(limits = c(-5, 4)) +
+  scale_x_continuous(limits = c(0, 2*pi)) +
+  scale_fill_manual(values = c("deepskyblue3", "deepskyblue4",
+                               "green3", "green4","tomato", "tomato2")) +
+  scale_alpha_identity() +
+  theme_void() +
+  theme(legend.position = "none") 
+
+# Look at plot
+p
+
+# Look what happens when you make it polar
+p + coord_polar()
 
 
 
 
+
+
+
+
+
+
+
+# Basement ----
+
+# Fit model on that using all explanatory variables
+model_all <- lm(score ~ ., data = df)
+
+# Check summary
+summary(model_all)
+
+# Check 'variance inflation factors' (VIFs)
+(vif_values <- car::vif(model_all))
+
+# Make a fun barplot to visualize these
+barplot(vif_values, main = "VIF Values", horiz = T, col = "steelblue")
+## Add a line at VIF 5 as that is 'rule of thumb' for "severe" correlation
+abline(v = 5, lwd = 3, lty = 2)
+
+# Can then remove terms over the threshold and re-check VIFs
+model_simp <- lm(score ~ play_hrs + study_hrs, data = df)
+barplot(car::vif(model_simp), main = "VIF Values (ver. 2)", horiz = T, col = "lightblue")
+
+# Can check for multi-colinearity
+exp_df <- df[2:4]
+
+# Get correlation matrix
+cor_mat <- stats::cor(x = exp_df)
+
+# Get the inverse correlation matrix too
+invcor_mat <- MASS::ginv(X = cor_mat)
+
+# Rename row / column names of this manually
+colnames(invcor_mat) <- names(exp_df)
+rownames(invcor_mat) <- names(exp_df)
+
+# Check that out
+invcor_mat
+
+# Make a nice correlation plot
+corrplot::corrplot(invcor_mat, method = 'number', is.corr = F)
+
+
+
+
+
+
+
+# Sub-Basement ----
 
 
 
