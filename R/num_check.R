7d3073c7252fc2dad8f99f9f64244551d967f90b
njlyon0 <njlyon@alumni.iastate.edu>
2023-10-18 14:38:20 -0700

    Fixed typo in `num_check` description

--- R/num_check.R
+++ R/num_check.R
@@ -2,8 +2,8 @@
 #' 
 #' @description Identifies any elements in the column(s) that would be changed to NA if `as.numeric` is used on the column(s). This is useful for quickly identifying only the "problem" entries of ostensibly numeric column(s) that is/are read in as a character.
 #'
-#' @param data (dataframe) object containing at least one column of supposed dates
-#' @param col (character or numeric) name(s) or column number(s) of the column(s) containing putative dates in the data object
+#' @param data (dataframe) object containing at least one column of supposed numbers
+#' @param col (character or numeric) name(s) or column number(s) of the column(s) containing putative numbers in the data object
 #'
 #' @return (list) malformed numbers from each supplied column in separate list elements
 #' @export
