236cae103166e20d0575f0e0b2df7c84db22eea2
njlyon0 <njlyon@alumni.iastate.edu>
2024-03-14 09:28:49 -0700

    Core script structure for `safe_rename` exploration

---
+++ dev/safe_rename.R
@@ -0,0 +1,27 @@
+## --------------------------------------------- ##
+# Safely Rename Columns
+## --------------------------------------------- ##
+
+# PURPOSE:
+## Safely rename columns via a function
+## Alternative (unsafe) is something like:
+### `names(df)[1] <- c("x")`
+
+## ----------------------------- ##
+# Housekeeping ----
+## ----------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, supportR)
+
+# Clear environment
+rm(list = ls())
+
+## ----------------------------- ##
+# Testing Arena ----
+## ----------------------------- ##
+
+
+
+
+# End ----
