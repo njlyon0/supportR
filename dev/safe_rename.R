d677644ebd82a942de635cb86c90d964a84ce950
njlyon0 <njlyon@alumni.iastate.edu>
2024-03-14 09:41:45 -0700

    Fleshed out a safe-r script way of renaming columns

--- dev/safe_rename.R
+++ dev/safe_rename.R
@@ -23,17 +23,23 @@ rm(list = ls())
 
 # Make a dataframe
 (df <- data.frame("first" = 1:3,
+                  "middle" = 4:6,
                   "second" = 7:9))
 
 # Duplicate to test various renaming approaches
-df3 <- df2 <- df
+df4 <- df3 <- df2 <- df
 
 # Rename in base R / unsafe manner
 names(df2)[1] <- "number_one"
 df2
 
+# Safe-r method
+## ID bad name
+bad_name <- c("first", "second")
 
-
+## Match and replace it in the names vector
+names(df3)[names(df3) %in% bad_name] <- c("safe_first", "safe_second")
+df3
 
 
 
