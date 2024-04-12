b7b353d1bfa1870d73c1b4136d38283f4f5a4ad5
njlyon0 <nicholasjlyon@gmail.com>
2024-04-12 11:40:22 -0400

    First complete pass at roxygen format for `tabularize_md`

--- R/tabularize_md.R
+++ R/tabularize_md.R
@@ -1,12 +1,21 @@
+#' @title Make a Markdown File into a Table
 #' 
-
-
-
+#' @description Accepts one markdown file (i.e., "md" file extension) and returns its content as a table. Nested heading structure in markdown file--as defined by hashtags / pounds signs (#)--is identified and preserved as columns in the resulting tabular format. Each line of non-heading content in the file is preserved in the right-most column of one row of the table.
+#'
+#' @param file (character) name and file path of markdown file to transform into a table
+#' 
+#' @return (dataframe) table with one additional column than there are heading levels in the document (e.g., if first and second level headings are in the document, the resulting table will have three columns) and one row per line of non-heading content in the markdwon file.
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @export
+#' 
+#' @examples
+#' \dontrun{
 #' # Invoke function
 #' tabularize_md(file = file.path("dev", "test-markdown.md"))
-
-
-# Define function
+#' }
+#' 
 tabularize_md <- function(file = NULL){
   
   # Error out if file isn't specified
