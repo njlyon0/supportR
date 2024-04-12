0271e2b3a123a865e9620ff15e4b2891bc9184fd
njlyon0 <nicholasjlyon@gmail.com>
2024-04-12 10:25:01 -0400

    First steps in exploring `tabularize_md` with a testng markdown file

--- dev/tabularize_md.R
+++ dev/tabularize_md.R
@@ -9,7 +9,7 @@
 ## Nested such that it is clear which set of headings a given set of contents is nested beneath
 
 ## --------------------------- ##
-# Housekeeping ----
+       # Housekeeping ----
 ## --------------------------- ##
 # Load desired libraries
 # install.packages("librarian")
@@ -18,8 +18,28 @@ librarian::shelf(tidyverse, devtools)
 # Clear environment
 rm(list = ls())
 
+## --------------------------- ##
+        # Exploration ----
+## --------------------------- ##
 
+# Read the lines of the test markdown file
+(text_v1 <- base::readLines(con = file.path("dev", "test-markdown.md")))
 
+# Remove all empty entries
+(text_v2 <- generics::setdiff(x = text_v1, y = ""))
+
+# Make into a dataframe
+(text_v3 <- data.frame("text" = text_v2))
+
+# Identify number of hashtags (i.e., heading level)
+text_v4 <- text_v3 %>% 
+  dplyr::mutate(level = stringr::str_count(string = text, pattern = "#"),
+                .before = dplyr::everything()) %>% 
+  # Coerce 0s to an artificially high number
+  dplyr::mutate(level = ifelse(test = (level == 0), yes = 999, no = level))
+
+# Check that out
+text_v4
 
 
 
---
+++ dev/test-markdown.md
@@ -0,0 +1,17 @@
+# Testing Markdown
+
+## First Level 2
+
+### Level 3
+
+Content for the first level 2 / level 3
+
+More content for the same heading chunk
+
+## Another level 2
+
+More content
+
+### Another level 3
+
+Still more content
