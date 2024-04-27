9c813617892f996f0afa6124abeed5f9620ea3fa
njlyon0 <njlyon@alumni.iastate.edu>
2024-04-27 12:36:22 -0400

    Removed code copy/pasted from elsewhere (examples of non-ASCII characters)

--- dev/fix_non_ascii.R
+++ dev/fix_non_ascii.R
@@ -21,46 +21,13 @@ rm(list = ls())
 ## ----------------------------------- ##
 
 # Make a vector of non-ASCII characters
-(bad_vec <- c("’"))
+(bad_vec <- c("’", "“", "”", "—", "−", "-", "–", "×", "ﬁ", "ö", " ", "  ", "  ", "­", "·", "…"))
+
+stringr::str_detect(string = bad_vec, pattern = "[^[:ascii:]]")
+
+
 
 
 
 
 # End ----
-
-
-dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                            .fns = \(t){gsub(pattern = , replacement = "'",
-                                             x = t)})) %>%
-  # Curly quotes are an issue too
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = "“|”", replacement = '"',
-                                               x = t)})) %>%
-  # Also non-hyphen dashes
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = "—|−|-|–", replacement = "-",
-                                               x = t)})) %>%
-  # Multiplication symbol
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = "×", replacement = "*",
-                                               x = t)})) %>%
-  # Non-ASCII letter fixes
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = "ﬁ", replacement = "fi",
-                                               x = t)})) %>%
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = "ö", replacement = "o",
-                                               x = t)})) %>%
-  # Worst of all, weird spaces
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = " |  |  |­|·", replacement = " ",
-                                               x = t)})) %>%
-  # Ellipses
-  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.character),
-                              .fns = \(t){gsub(pattern = "…", replacement = "...",
-                                               x = t)})) %>%
-  # Re-count non-ASCII characters
-  dplyr::rowwise() %>%
-  dplyr::mutate(non_ascii_ct = sum(stringr::str_detect(dplyr::c_across(dplyr::where(is.character)),
-                                                       pattern = "[^[:ascii:]]"),
-                                   na.rm = T))
