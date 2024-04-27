b1b10e4e2300d1d7c5c3a23a62f5d4ca8dd5ba75
njlyon0 <njlyon@alumni.iastate.edu>
2024-04-27 12:41:14 -0400

    Initial function skeleton (without guts)

--- dev/fix_non_ascii.R
+++ dev/fix_non_ascii.R
@@ -21,13 +21,36 @@ rm(list = ls())
 ## ----------------------------------- ##
 
 # Make a vector of non-ASCII characters
-(bad_vec <- c("’", "“", "”", "—", "−", "-", "–", "×", "ﬁ", "ö", " ", "  ", "  ", "­", "·", "…"))
+(bad_vec <- c("’", "“", "”", "—", "−", "–", "×", "ﬁ", "ö", "­", "·", "…"))
 
+# Check to make sure they are all non-ASCII characters
 stringr::str_detect(string = bad_vec, pattern = "[^[:ascii:]]")
 
+# Define function
+fix_non_ascii <- function(x = NULL){
+  
+  # Error out if x isn't supplied
+  if(is.null(x) == TRUE)
+    stop("'x' must be specified")
+  
+  # Error out if x isn't a character
+  if(is.character(x) != TRUE)
+    stop("'x' must be a character")
+  
+  # Make a new object for ease of later manipulations
+  q <- x
+  
+  # Do actually fixing
+  
+  
+  # Return that fixed vector
+  return(q) }
 
+# Invoke function
+(good_vec <- fix_non_ascii(x = bad_vec))
 
-
-
+# Check to see if that worked
+## Conditional is a double negative ('not not ASCII')
+any(stringr::str_detect(string = good_vec, pattern = "[^[:ascii:]]") != TRUE)
 
 # End ----
