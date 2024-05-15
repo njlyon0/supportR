93f333c28f70be053d0077b7a38266bbf2e7bf99
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-15 10:36:59 -0400

    First pass at function

--- dev/count.R
+++ dev/count.R
@@ -10,7 +10,45 @@ librarian::shelf(tidyverse)
 rm(list = ls())
 
 # Make a test vector
-(test_vec <- c(rep("a", times = 5), rep("x", times = 2),  rep("33", times = 6), 11))
+(test_vec <- c(rep("a", times = 5), rep("x", times = 2),  rep("33", times = 6), 11, rep(NA, times = 3)))
+
+# Define function
+count <- function(vec = NULL){
+  
+  # Error out for missing vector
+  if(is.null(vec) == TRUE)
+    stop("'vec' must be specified")
+  
+  # Error out for non-vector
+  if(is.vector(vec) != TRUE)
+    stop("'vec' must be a vector")
+  
+  # Identify unique bits
+  unique_vec <- unique(vec)
+  
+  # Make a dataframe to store counts
+  count_df <- data.frame("value" = unique_vec,
+                         "count" = NA)
+  
+  # Count number of occurrences of each unique element
+  for(bit in unique_vec){
+    
+    # Actual bit counting
+    bit_ct <- length(vec[vec == bit])
+    
+    # Add to dataframe
+    count_df[count_df$value == bit, ]$count <- bit_ct
+    
+    }
+
+  # Return that dataframe
+  return(count_df) }
+
+# Invoke function
+count(vec = test_vec)
+
+
+
 
 # Identify unique bits
 (unique_vec <- unique(x = test_vec))
