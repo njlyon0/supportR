bbbaf882dc17534c366945a80347c608543cdbb8
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-15 10:51:43 -0400

    Deleting non-function exploration

--- dev/count.R
+++ dev/count.R
@@ -62,25 +62,3 @@ count <- function(vec = NULL){
 count(vec = test_vec)
 
 
-
-
-# Identify unique bits
-(unique_vec <- unique(x = test_vec))
-
-# Make a dataframe for that
-count_df <- data.frame("value" = unique_vec,
-                       "count" = NA)
-
-# Work on counting elements
-for(bit in unique_vec){
-  
-  # Count number of occurrences
-  bit_ct <- length(test_vec[test_vec == bit])
-  
-  # Add to dataframe
-  count_df[count_df$value == bit, ]$count <- bit_ct
-  
-}
-
-
-
