1c8854fc5e071d86f0bd9b7dccdd32bf9a1e94ee
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-15 10:51:26 -0400

    Tweaked function to correctly handle NAs in vector

--- dev/count.R
+++ dev/count.R
@@ -11,6 +11,7 @@ rm(list = ls())
 
 # Make a test vector
 (test_vec <- c(rep("a", times = 5), rep("x", times = 2),  rep("33", times = 6), 11, rep(NA, times = 3)))
+# vec <- test_vec
 
 # Define function
 count <- function(vec = NULL){
@@ -33,14 +34,27 @@ count <- function(vec = NULL){
   # Count number of occurrences of each unique element
   for(bit in unique_vec){
     
-    # Actual bit counting
-    bit_ct <- length(vec[vec == bit])
-    
-    # Add to dataframe
-    count_df[count_df$value == bit, ]$count <- bit_ct
-    
-    }
-
+    # Handle instances of NA in original vector
+    if(is.na(bit) == TRUE){
+      
+      # Actual bit counting
+      bit_ct <- sum(is.na(vec))
+      
+      # Add to dataframe
+      count_df[is.na(count_df$value), ]$count <- bit_ct
+      
+      # If not NA...
+    } else {
+      
+      # Count occurrences
+      bit_ct <- length(vec[vec == bit])
+      
+      # Add to dataframe
+      count_df[(count_df$value == bit & !is.na(count_df$value)), ]$count <- bit_ct
+      
+    } # Close conditional
+  } # Close loop
+  
   # Return that dataframe
   return(count_df) }
 
