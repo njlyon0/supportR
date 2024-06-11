4d9143cbb5a8e237892cae09e9c42dd31cbb7d67
njlyon0 <njlyon@alumni.iastate.edu>
2024-06-11 11:06:47 -0400

    Getting bits of example from specific ordination functions to make testing simpler

--- dev/ordination.R
+++ dev/ordination.R
@@ -11,11 +11,33 @@
 ## ------------------------------------ ##
 
 # Library loading
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, vegan)
 devtools::load_all()
 
 # Clear environment
 rm(list = ls())
+
+# Use data from the vegan package
+utils::data("varespec", package = 'vegan')
+resp <- varespec
+
+# Make some columns of known number of groups
+factor_4lvl <- c(rep.int("Trt1", (nrow(resp)/4)),
+                 rep.int("Trt2", (nrow(resp)/4)),
+                 rep.int("Trt3", (nrow(resp)/4)),
+                 rep.int("Trt4", (nrow(resp)/4)))
+
+# And combine them into a single data object
+data <- cbind(factor_4lvl, resp)
+
+# Get a distance matrix from the data
+dist <- vegan::vegdist(resp, method = 'kulczynski')
+
+# Perform a PCoA on the distance matrix to get points for an ordination
+pcoa_mod <- ape::pcoa(dist)
+
+# Actually perform multidimensional scaling
+nms_mod <- vegan::metaMDS(data[-1], autotransform = FALSE, expand = FALSE, k = 2, try = 50)
 
 ## ------------------------------------ ##
 # Explore ----
