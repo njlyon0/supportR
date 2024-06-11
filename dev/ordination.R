2258971547ff5b040f24672bfa07154cedb4d954
njlyon0 <njlyon@alumni.iastate.edu>
2024-06-11 12:00:37 -0400

    Translate generic ordination into real function

--- dev/ordination.R
+++ dev/ordination.R
@@ -46,101 +46,99 @@ class(nms_mod)
 # Explore ----
 ## ------------------------------------ ##
 
-# Define arguments
-grps <- data$factor_4lvl
-# title <- NA
-grp_colors <- c('#41b6c4', '#c51b7d', '#7fbc41', '#d73027', '#4575b4',
-           '#e08214', '#8073ac', '#f1b6da', '#b8e186', '#8c96c6')
-grp_shapes <- rep(x = 21:25, times = 2)
-grp_lines <- rep(x = 1, times = 10)
-
-## cex
-pt_size = 1.5
-
-## scales::alpha(..., alpha)
-pt_alpha <- 1
-
-## cex.lab
-# lab_text_size = 1.25
-
-## cex.axis
-# axis_text_size = 1
-
-## Legend entries
-leg_pos = 'bottomleft'
-leg_cont = unique(grps)
-
-# Define model
-# mod <- nms_mod
-mod <- pcoa_mod
-
-# Model-specific stuff
-# Identify axis labels (ordination type-dependent)
-## Principal Coordinates Analysis
-if(all(class(mod) == "pcoa")){
+# Define function
+ordination <- function(
+    mod = NULL,
+    grps = NULL, 
+    grp_colors = c('#41b6c4', '#c51b7d', '#7fbc41', '#d73027', '#4575b4',
+                   '#e08214', '#8073ac', '#f1b6da', '#b8e186', '#8c96c6'), 
+    grp_shapes = rep(x = 21:25, times = 2), 
+    grp_lines = rep(x = 1, times = 10),
+    pt_size = 1.5,
+    pt_alpha = 1,
+    lab_text_size = 1.25,
+    axis_text_size = 1,
+    leg_pos = "bottomleft",
+    leg_cont = unique(grps)
+){
   
-  # Define actual model object (with plot-able data points)
-  mod_actual <- mod$vectors
-  mod_points <- mod_actual
+  # Model-specific stuff
+  # Identify axis labels (ordination type-dependent)
+  ## Principal Coordinates Analysis
+  if(all(class(mod) == "pcoa")){
+    
+    # Define actual model object (with plot-able data points)
+    mod_actual <- mod$vectors
+    mod_points <- mod_actual
+    
+    # Create informative plot labels
+    xlab_actual <- paste0("PC1 (", round(mod$values$Relative_eig[1] * 100, digits = 2), "%)")
+    ylab_actual <- paste0("PC2 (", round(mod$values$Relative_eig[2] * 100, digits = 2), "%)")
+    leg_title <- NULL
+  }
+  ## Non-Metric Multidimensional Scaling
+  if(all(class(mod) %in% c("metaMDS", "monoMDS"))){
+    
+    # Define actual model object (with plot-able data points)
+    mod_actual <- mod
+    mod_points <- mod$points
+    
+    # Create informative axis labels
+    xlab_actual <- "NMS Axis 1"
+    ylab_actual <- "NMS Axis 2"
+    leg_title <- paste0("Stress = ", round(mod_actual$stress, digits = 3))
+  }
   
-  # Create informative plot labels
-  xlab_actual <- paste0("PC1 (", round(mod$values$Relative_eig[1] * 100, digits = 2), "%)")
-  ylab_actual <- paste0("PC2 (", round(mod$values$Relative_eig[2] * 100, digits = 2), "%)")
-  leg_title <- NULL
-}
-## Non-Metric Multidimensional Scaling
-if(all(class(mod) %in% c("metaMDS", "monoMDS"))){
+  # Identify unique groups in dat
+  (grp_names <- unique(grps))
   
-  # Define actual model object (with plot-able data points)
-  mod_actual <- mod
-  mod_points <- mod$points
+  # Assign names to the vectors of colors/shapes/lines
+  (names(grp_colors) <- grp_names)
+  (names(grp_shapes) <- grp_names)
+  (names(grp_lines) <- grp_names)
   
-  # Create informative axis labels
-  xlab_actual <- "NMS Axis 1"
-  ylab_actual <- "NMS Axis 2"
-  leg_title <- paste0("Stress = ", round(mod_actual$stress, digits = 3))
-}
-
-# Identify unique groups in dat
-(grp_names <- unique(grps))
-
-# Assign names to the vectors of colors/shapes/lines
-(names(grp_colors) <- grp_names)
-(names(grp_shapes) <- grp_names)
-(names(grp_lines) <- grp_names)
-
-# Crop all three vectors to the length of groups in the data
-## Also adjust color opacity
-(colors_actual <- grp_colors[is.na(names(grp_colors)) != TRUE])
-(shapes_actual <- grp_shapes[is.na(names(grp_shapes)) != TRUE])
-(lines_actual <- grp_lines[is.na(names(grp_lines)) != TRUE])
-
-# Create blank plot
-graphics::plot(x = mod_actual, type = 'n', col = 'white', pch = 1,
-               xlab = xlab_actual, ylab = ylab_actual)
-
-# For each group in the ordination
-for(focal_grp in grp_names){
+  # Crop all three vectors to the length of groups in the data
+  ## Also adjust color opacity
+  (colors_actual <- grp_colors[is.na(names(grp_colors)) != TRUE])
+  (shapes_actual <- grp_shapes[is.na(names(grp_shapes)) != TRUE])
+  (lines_actual <- grp_lines[is.na(names(grp_lines)) != TRUE])
   
-  # Add points
-  graphics::points(x = mod_points[grp_names == focal_grp, 1],
-                   y = mod_points[grp_names == focal_grp, 2],
-                   pch = shapes_actual[focal_grp], 
-                   bg = scales::alpha(colour = colors_actual[focal_grp], alpha = pt_alpha),
-                   cex = pt_size)
+  # Create blank plot
+  graphics::plot(x = mod_actual, type = 'n', col = 'white', pch = 1,
+                 xlab = xlab_actual, 
+                 ylab = ylab_actual,
+                 cex.lab = lab_text_size,
+                 cex.axis = axis_text_size)
   
+  # For each group in the ordination
+  for(focal_grp in grp_names){
+    
+    # Add points
+    graphics::points(x = mod_points[grp_names == focal_grp, 1],
+                     y = mod_points[grp_names == focal_grp, 2],
+                     pch = shapes_actual[focal_grp], 
+                     bg = scales::alpha(colour = colors_actual[focal_grp], alpha = pt_alpha),
+                     cex = pt_size)
+    
   } # Close points loop
+  
+  # With all of the points plotted, add ellipses of matched colors
+  # This also allows for variation in line type if desired
+  vegan::ordiellipse(ord = mod_actual, groups = grps, col = colors_actual,
+                     display = 'sites', kind = 'sd', lwd = 2,
+                     lty = lines_actual, label = FALSE)
+  
+  # Finally, add a legend
+  graphics::legend(x = leg_pos, legend = leg_cont, bty = "n", title = leg_title,
+                   pt.cex = 1.25, pch = shapes_actual, cex = 1.15, pt.bg = colors_actual)
+  
+} # Close function
 
-# With all of the points plotted, add ellipses of matched colors
-# This also allows for variation in line type if desired
-vegan::ordiellipse(ord = mod_actual, groups = grps, col = colors_actual,
-                   display = 'sites', kind = 'sd', lwd = 2,
-                   lty = lines_actual, label = FALSE)
-
-# Finally, add a legend
-graphics::legend(x = leg_pos, legend = leg_cont, bty = "n", title = leg_title,
-                 pt.cex = 1.25, pch = shapes_actual, cex = 1.15, pt.bg = colors_actual)
-
+# Invoke function
+## PCoA variant
+ordination(mod = pcoa_mod, grps = data$factor_4lvl)
+## NMS variant
+ordination(mod = nms_mod, grps = data$factor_4lvl)
 
 # End ----
 
