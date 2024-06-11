b566567822d3b480254587a8f971f15baa820566
njlyon0 <njlyon@alumni.iastate.edu>
2024-06-11 11:04:29 -0400

    Script skeleton for generic ordination function

---
+++ dev/ordination.R
@@ -0,0 +1,27 @@
+## --------------------------------------------------- ##
+# General Ordination Function
+## --------------------------------------------------- ##
+
+# PURPOSE
+## Dev area for a generic 'ordination' function to supersede `pcoa_ord` and `nms_ord`
+## Plus explore use of `...` parameter to simplify arguments of this function
+
+## ------------------------------------ ##
+# Housekeeping ----
+## ------------------------------------ ##
+
+# Library loading
+librarian::shelf(tidyverse)
+devtools::load_all()
+
+# Clear environment
+rm(list = ls())
+
+## ------------------------------------ ##
+# Explore ----
+## ------------------------------------ ##
+
+
+
+
+# End ----
