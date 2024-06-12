d42e79c0657b9570f8db2d5e37a25b69a6ca4969
njlyon0 <njlyon@alumni.iastate.edu>
2024-06-12 15:07:49 -0400

    Moving `ordination` to package build and creating empty script for related unit testing

---
+++ tests/testthat/test-ordination.R
@@ -0,0 +1,22 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-ordination.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  expect_error()
+})
+
+# Warning testing
+test_that("Warnings work as desired", {
+  expect_warning()
+})
+
+# Message testing
+test_that("Messages work as desired", {
+  expect_message()
+})
+
+# Output testing
+test_that("Outputs are as expected", {
+  expect_equal()
+})
