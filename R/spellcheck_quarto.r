faa72d2c88c32253cb6de51334c0a36fea4640fa
njlyon0 <nicholasjlyon@gmail.com>
2026-03-10 15:45:17 -0400

    feat (quarto spellcheck): move to package, change to function syntax, add error checks

--- R/spellcheck_quarto.r
+++ R/spellcheck_quarto.r
@@ -1,10 +1,21 @@
-
-# Load libraries
-library(spelling); library(magrittr)
-
-
-# Define function
+#' @title Check the Spelling of All Quarto Files in a Given Directory
+#' 
+#' @description Runs `spelling::spell_check_files` on all Quarto (.qmd) files in folders that would be rendered when rendering a Quarto project (i.e., folders that do not begin with an underscore). 
+#' 
+#' @param path (character) directory in which to identify all Quarto files are to be identified and spell-checked. Recursively identifies Quarto files in folders within the top-level folder. Unrendered folders are ignored. Defaults to current working directory
+#' @param quiet (logical) whether to message the name of the current file being spell-checked as it is checked. Defaults to `FALSE`
+#' 
+#' @return (dataframe) table containing a column for the Quarto file, its path, words flagged as typos by `spell_check_files`, and what line(s) of the file that typo was found. One row per typo. If found on multiple lines in a single file, concatenated with comma as separators.
+#' 
+#' @export
+#' 
 spellcheck_quarto <- function(path = getwd(), quiet = FALSE){
+  # Squelch 'visible bindings' note
+  files <- lines <- NULL
+
+  # Error checks for 'path'
+  if(is.null(path) || is.character(path) != TRUE || length(path) != 1)
+    stop("'path' must be a single file path specified as a character")
 
   # Warning for bad value to 'quiet'
   if(is.logical(quiet) != TRUE){
@@ -67,5 +78,4 @@ spellcheck_quarto <- function(path = getwd(), quiet = FALSE){
   # Return that
   return(spell_df) }
 
-# Invoke function
-spellcheck_quarto(quiet = F)
+# End ----
