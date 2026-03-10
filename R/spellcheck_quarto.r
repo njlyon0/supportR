6edd233f0bf327337b6d4a7d04f104748caa11e2
njlyon0 <nicholasjlyon@gmail.com>
2026-03-10 16:04:07 -0400

    fix (typo): resolve grammar issue

--- R/spellcheck_quarto.r
+++ R/spellcheck_quarto.r
@@ -2,7 +2,7 @@
 #' 
 #' @description Runs `spelling::spell_check_files` on all Quarto (.qmd) files in folders that would be rendered when rendering a Quarto project (i.e., folders that do not begin with an underscore). 
 #' 
-#' @param path (character) directory in which to identify all Quarto files are to be identified and spell-checked. Recursively identifies Quarto files in folders within the top-level folder. Unrendered folders are ignored. Defaults to current working directory
+#' @param path (character) directory in which all Quarto files are to be identified and spell-checked. Recursively identifies Quarto files in folders within the top-level folder. Unrendered folders are ignored. Defaults to current working directory
 #' @param quiet (logical) whether to message the name of the current file being spell-checked as it is checked. Defaults to `FALSE`
 #' 
 #' @return (dataframe) table containing a column for the Quarto file, its path, words flagged as typos by `spell_check_files`, and what line(s) of the file that typo was found. One row per typo. If found on multiple lines in a single file, concatenated with comma as separators.
